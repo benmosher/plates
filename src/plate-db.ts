01d9d1357591364642915241e56c4d775ea0202d
Ben Mosher <me@benmosher.com>
2025-09-20 14:40:11 -0400

    cleanup logs, use map for in-memory state

--- src/plate-db.ts
+++ src/plate-db.ts
@@ -1,7 +1,6 @@
 // create a database
 
 import { use, useCallback, useMemo, useState } from "react";
-import { useImmer } from "use-immer";
 
 export interface Plate {
   count?: number;
@@ -15,7 +14,7 @@ export interface Plate {
   color: string;
 }
 
-const PLATES_DEFAULT: readonly Plate[] = [
+const INITIAL_PLATES: readonly Plate[] = [
   { weight: 0.25, thicknessMm: 8, diameterMm: 57, color: "#62D926", count: 1 },
   { weight: 0.5, thicknessMm: 9, diameterMm: 60, color: "#FFBF00", count: 1 },
   { weight: 0.75, thicknessMm: 10, diameterMm: 60, color: "#3C71F7", count: 1 },
@@ -47,19 +46,16 @@ function initializeStore<T>(
   return new Promise((resolve, reject) => {
     request.onsuccess = function () {
       if (this.result.length === 0) {
-        console.log(`initializing ${storeName} store`);
         for (const item of defaultData) {
           store.add(item);
         }
         txn.oncomplete = function () {
-          console.log(`${storeName} store initialized`);
           resolve(defaultData);
         };
         txn.onerror = function () {
           reject(this.error);
         };
       } else {
-        console.log(`${storeName} store already initialized`);
         resolve(this.result as readonly T[]);
       }
       txn.commit();
@@ -83,25 +79,23 @@ function initializeDatabase(): Promise<Initialized> {
 
     // capture the database
     request.onsuccess = function () {
-      console.log("database opened, checking data");
-      initializeStore(this.result, "plates", PLATES_DEFAULT).then((plates) => {
-        console.log("database initialized");
+      initializeStore(this.result, "plates", INITIAL_PLATES).then((plates) => {
         resolve({ db: this.result, plates });
-      });
+      }, reject);
     };
 
     /** initialize the data */
     request.onupgradeneeded = function () {
-      console.log("database upgrade needed");
       const db = this.result;
 
-      // TODO: handle errors? note completion?
+      // TODO: handle errors?
       if (!db.objectStoreNames.contains("plates")) {
-        console.log("creating plates store");
         db.createObjectStore("plates", { keyPath: "weight" });
-        console.log("plates store created and initialized");
       }
-      console.log("database upgrade complete");
+    };
+
+    request.onerror = function () {
+      reject(this.error);
     };
   });
 }
@@ -122,36 +116,31 @@ export function useMassStorage(): MassStorage {
   const { db, plates: initialDbPlates } = use(dbPromise);
 
   // db does not send change events, so we must manage the plate state ourselves
-  const [plates, setPlates] = useImmer(
-    initialDbPlates.toSorted((a, b) => a.weight - b.weight)
+  // todo: useSyncExternalStore?
+  const [plateMap, updatePlateMap] = useState(
+    () => new Map<number, Plate>(initialDbPlates.map((p) => [p.weight, p]))
   );
 
   const putPlate = useCallback(
     (plate: Plate) => {
-      setPlates((draft) => {
-        const index = draft.findIndex((p) => p.weight === plate.weight);
-        if (index >= 0) {
-          draft[index] = plate;
-        } else {
-          draft.push(plate);
-          draft.sort((a, b) => a.weight - b.weight);
-        }
+      updatePlateMap((m) => {
+        return new Map(m).set(plate.weight, plate);
       });
       const txn = db.transaction("plates", "readwrite");
       const store = txn.objectStore("plates");
-      const putRequest = store.put(plate);
-      putRequest.onsuccess = function () {
-        console.log(`plate ${plate.weight} saved`);
-      };
-      putRequest.onerror = function () {
-        console.error(
-          `error saving plate ${plate.weight}: ${this.error?.message}`
-        );
-      };
+      store.put(plate);
       txn.commit();
     },
     [db]
   );
 
-  return useMemo(() => ({ plates, putPlate }), [plates, putPlate]);
+  return useMemo(
+    () => ({
+      plates: Array.from(plateMap.values()).toSorted(
+        (a, b) => a.weight - b.weight
+      ),
+      putPlate,
+    }),
+    [plateMap, putPlate]
+  );
 }
