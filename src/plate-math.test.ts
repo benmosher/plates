950d70c230081bd246e6f2a02c64420357444ca2
Ben Mosher <me@benmosher.com>
2025-09-21 15:01:35 -0400

    fix tests

--- src/plate-math.test.ts
+++ src/plate-math.test.ts
@@ -21,42 +21,56 @@ describe("plates needed", () => {
       { weight: 5, count: 4 },
       { weight: 10, count: 4 },
     ];
-    expect(determinePlates(62.5, 12.5, plates)).toEqual([
+    expect(determinePlates(62.5, { weight: 12.5 }, plates)).toEqual([
       { weight: 10, count: 2 },
       { weight: 5, count: 1 },
     ]);
   });
 });
 
+function singles(weights: number[]) {
+  return weights.map((w) => ({ weight: w, count: 1 }));
+}
+
 describe("plate math", () => {
   test("basic cases", () => {
-    expect(determinePlateCombos([1, 2])).toEqual([0, 2, 4, 6]);
-    expect(determinePlateCombos([10])).toEqual([0, 20]);
+    expect(determinePlateCombos(singles([1, 2]))).toEqual([0, 2, 4, 6]);
+    expect(determinePlateCombos(singles([10]))).toEqual([0, 20]);
   });
   test("duplicate plates", () => {
-    expect(determinePlateCombos([10, 10])).toEqual([0, 20, 40]);
-    expect(determinePlateCombos([10, 10, 10])).toEqual([0, 20, 40, 60]);
+    expect(determinePlateCombos([{ weight: 10, count: 2 }])).toEqual([
+      0, 20, 40,
+    ]);
+    expect(determinePlateCombos([{ weight: 10, count: 3 }])).toEqual([
+      0, 20, 40, 60,
+    ]);
   });
   test("fractionals", () => {
-    expect(determinePlateCombos([0.25, 0.5, 0.75, 1])).toEqual(
+    expect(determinePlateCombos(singles([0.25, 0.5, 0.75, 1]))).toEqual(
       range(0, 5, 0.5)
     );
   });
   test("full set", () => {
-    expect(determinePlateCombos([0.25, 0.5, 0.75, 1, 2.5, 5, 10])).toEqual(
-      range(0, 40, 0.5)
-    );
+    expect(
+      determinePlateCombos(singles([0.25, 0.5, 0.75, 1, 2.5, 5, 10]))
+    ).toEqual(range(0, 40, 0.5));
   });
 });
 describe("weight space", () => {
   test("full set with handle", () => {
     expect(
-      determineWeightSpace(12.5, [0.25, 0.5, 0.75, 1, 2.5, 5, 10])
+      determineWeightSpace(
+        [{ weight: 12.5 }],
+        singles([0.25, 0.5, 0.75, 1, 2.5, 5, 10])
+      )
     ).toEqual(range(12.5, 52.5, 0.5));
   });
   test("full set with 4 10s, with handle", () => {
     expect(
-      determineWeightSpace(12.5, [0.25, 0.5, 0.75, 1, 2.5, 5, 10, 10, 10, 10])
+      determineWeightSpace(
+        [{ weight: 12.5 }],
+        [...singles([0.25, 0.5, 0.75, 1, 2.5, 5]), { weight: 10, count: 4 }]
+      )
     ).toEqual(range(12.5, 112.5, 0.5));
   });
 });
