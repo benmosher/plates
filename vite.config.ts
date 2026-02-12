45532aa57d84fbef0a221e9b0629a354717db2da
Ben Mosher <me@benmosher.com>
2026-02-12 17:00:35 -0500

    start dev server with /plates/ by moving it to config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/plates/",
   plugins: [react()],
   build: {
     manifest: true,
